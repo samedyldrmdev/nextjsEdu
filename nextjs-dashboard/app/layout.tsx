ae6cc45456d3858ae71e00961ff1b6cd02a80876
samettyldrm <smtyldrm32@gmail.com>
2024-01-13 11:34:36 +0100

    Update layout.tsx

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,20 +1,10 @@
-// layout.tsx
-
-import React from 'react';
-import { ReactNode } from 'react';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
-interface RootLayoutProps {
-  children: ReactNode;
-}
-
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+export default function RootLayout({children}: {children: React.ReactNode;}) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
-};
-
-export default RootLayout;
+}
