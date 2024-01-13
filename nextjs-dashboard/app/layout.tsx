873c4ef83af718f4752a72c33167c3da4adc4859
samettyldrm <smtyldrm32@gmail.com>
2024-01-13 11:05:43 +0100

    Update layout.tsx

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,14 +1,10 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode;}) {
   return (
     <html lang="en">
-      <body className={'${inter.className} antialiased'}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
