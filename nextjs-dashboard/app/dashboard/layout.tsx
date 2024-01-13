f03129ddaf3469ed283077b300455f869e730dbd
samettyldrm <smtyldrm32@gmail.com>
2024-01-13 12:09:08 +0100

    Update layout.tsx

--- nextjs-dashboard/app/dashboard/layout.tsx
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav'
 import React from 'react';
 
-export default function Layout({ children } : { children: React, ReactNode}) {
+export default function Layout({ children } : { children: React.ReactNode}) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
