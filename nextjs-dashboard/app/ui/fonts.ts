a32dcc8d15252b1189a1fa385349276fe6b29014
samettyldrm <smtyldrm32@gmail.com>
2024-01-13 01:26:30 +0100

    nextjs Education

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana, Montserrat } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+
+export const montserrat = Montserrat({
+    subsets: ['latin']
+})
