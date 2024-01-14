eb4c415ff8fcb4123ab1f78c5194ba8dbb1ce2f7
samettyldrm <smtyldrm32@gmail.com>
2024-01-14 01:18:33 +0100

    update

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -26,7 +26,6 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <p>Samet YILDIRIM</p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
